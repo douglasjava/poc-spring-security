1427f5146b7cd02fb7e62d64e3fe86dd0ec33681
Douglas Dias <douglasmarquesdias@hotmail.com>
2019-03-16 17:32:35 -0300

    'spring-security'

---
+++ src/main/java/com/algaworks/gp/config/DataBaseConfig.java
@@ -0,0 +1,68 @@
+package com.algaworks.gp.config;
+
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@EnableAutoConfiguration
+public class DataBaseConfig {
+
+	@Autowired
+	private Environment env;
+
+	@Bean
+	@ConfigurationProperties("app.datasource")
+	public DataSource dataSource() {
+		return DataSourceBuilder.create().build();
+	}
+	
+	@Bean
+	public EntityManagerFactory entityManagerFactory() {
+		
+		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		vendorAdapter.setDatabase(Database.H2);
+		vendorAdapter.setGenerateDdl(false);
+		
+		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		em.setDataSource(dataSource());
+		em.setJpaVendorAdapter(vendorAdapter);
+		em.setPackagesToScan(packagesToScan);
+		em.setJpaProperties(additionalProperties());
+		em.afterPropertiesSet();
+		
+		return em.getObject();
+		
+	}
+
+	private Properties additionalProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.hbm2ddl.auto", env.getProperty("spring.jpa.show-sql"));
+		properties.setProperty("hibernate.hbm2ddl.auto", env.getProperty("spring.jpa.hibernate.ddl-auto"));
+		properties.setProperty("hibernate.dialect", env.getProperty("spring.jpa.properties.hibernate.dialect"));
+		properties.setProperty("hibernate.format_sql", env.getProperty("spring.jpa.properties.hibernate.format_sql"));
+		properties.setProperty("hibernate.show-sql", env.getProperty("spring.jpa.properties.hibernate.show-sql"));
+		return properties;
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(emf);
+		return transactionManager;
+	}
+}
---
+++ src/main/java/com/algaworks/gp/config/JdbcSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.algaworks.gp.config;
+
+import static com.algaworks.gp.utils.JdbcUtils.PERMISSOES_POR_GRUPO;
+import static com.algaworks.gp.utils.JdbcUtils.PERMISSOES_POR_USUARIO;
+import static com.algaworks.gp.utils.JdbcUtils.USUARIO_POR_LOGIN;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+//@Configuration
+public class JdbcSecurityConfig {
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder builder, 
+			PasswordEncoder passwordEncoder, 
+			DataSource dataSource) throws Exception {
+		builder
+			.jdbcAuthentication()
+			.dataSource(dataSource)
+			.passwordEncoder(passwordEncoder)
+			.usersByUsernameQuery(USUARIO_POR_LOGIN)
+			.authoritiesByUsernameQuery(PERMISSOES_POR_USUARIO)
+			.groupAuthoritiesByUsername(PERMISSOES_POR_GRUPO);
+	}
+}
